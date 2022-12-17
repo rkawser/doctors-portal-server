0561080629b76e6bbe7256b7275dbb5930e88034
raihan kawser <raihankawser55@gmail.com>
2022-12-17 12:05:03 +0600

    update server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors')
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 
 app.use(cors())
@@ -11,23 +12,151 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rspxj2l.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }); 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
- 
-async function  run(){
-    try{
-            await client.connect();
 
-            const serviceCollection = client.db('doctors_portal').collection('services')
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
 
-            app.get('/services',async (req,res)=>{
-                  const query = {};
-                  const cursor = serviceCollection.find(query)
-                  const result = await cursor.toArray()
-                  res.send(result)
-            })
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthrized access' })
     }
-    finally{
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
+async function run() {
+    try {
+        await client.connect();
+
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
+        const userCollection = client.db('doctors_portal').collection('users');
+
+
+        //admin api
+        app.put('/users/admin/:email', verifyJwt, async (req, res) => {
+            const email = req?.params.email;
+            const requester = req?.decoded.email;
+            
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email }
+                const updateDoc = {
+                    $set: { role: 'admin' }
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' });
+            }
+
+        })
+
+
+        //admin api 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({admin :isAdmin});
+        })
+
+
+        app.put('/users/:email', async (req, res) => {
+            const email = req?.params.email;
+            const user = req.body;
+            const filter = { email: email }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ result, token });
+        })
+
+        app.get('/user', verifyJwt, async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        //booking 
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { teatMentName: booking.teatMentName, date: booking.date, patient: booking.patient };
+
+            const exists = await bookingCollection.findOne(query)
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result })
+        })
+
+        //get booking
+
+        app.get('/mybooking', verifyJwt, async (req, res) => {
+            const patient = req.query.patient;
+            const decodedEmail = req?.decoded?.email
+            
+            if (patient === decodedEmail) {
+                const query = { patient: patient }
+                const booking = await bookingCollection.find(query).toArray();
+                return res.send(booking)
+            } else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
+
+        })
+
+
+
+        //available
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            const services = await serviceCollection.find().toArray();
+            const query = { date: date };
+            const booking = await bookingCollection.find(query).toArray();
+
+            services.forEach(service => {
+                const serviceBooking = booking.filter(book => book.teatMentName === service.name);
+                const booked = serviceBooking.map(book => book.slot);
+                const available = service.slots.filter(slot => !booked.includes(slot));
+
+                service.slots = available;
+
+            })
+
+            res.send(services)
+        })
+
+
+    }
+    finally {
         //client.close();
     }
 }
